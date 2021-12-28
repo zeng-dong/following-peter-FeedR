3a6a8bc5ef416ee4c3ac6a2f92aa4851d85a706b
Dong Zeng <dong@freshbyte.com>
2021-12-28 15:56:22 -0600

    started using Yarp

--- src/Gateway/FeedR.Gateway/Program.cs
+++ src/Gateway/FeedR.Gateway/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("yarp"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "FeedR Gateway");
+app.MapReverseProxy();
 
 app.Run();
